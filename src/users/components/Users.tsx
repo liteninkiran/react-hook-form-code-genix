270a88cecf5441de7798b483e45ceb3aa6007fcd
Kiran Anand <liteninkiran@hotmail.com>
2024-09-08 15:21:41 +0100

    Old Way

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+
 export const Users = () => {
+  const [input, setInput] = useState('Default Value');
   return (
-    <div>Users</div>
+    <input value={input} placeholder={'Enter a value'} onChange={(e) => setInput(e.target.value) } />
   );
 }
