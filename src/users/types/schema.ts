414b2ad155dccb2c57e3e40f471cd522e7f20774
Kiran Anand <liteninkiran@hotmail.com>
2024-10-14 15:46:39 +0100

    Refine Zod Schema

--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
+import { patterns } from '../../constants';
+
 export const schema = z.object({
-    name: z.string().min(1, { message: 'Required' }),
-    email: z.string().min(1, { message: 'Email is required' }),
+  name: z.string().min(1, { message: 'Required' }),
+  email: z.string()
+    .min(1, { message: 'Email is required' })
+    .refine((text) => patterns.email.test(text), {
+      message: 'Email not valid',
+    }),
 });
