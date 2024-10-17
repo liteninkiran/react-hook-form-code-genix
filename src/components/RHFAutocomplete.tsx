32aebf85034bbb3f1197bb7b10ac5922b504c97a
Kiran Anand <liteninkiran@hotmail.com>
2024-10-17 16:44:03 +0100

    Generics

--- src/components/RHFAutocomplete.tsx
+++ src/components/RHFAutocomplete.tsx
@@ -1,17 +1,62 @@
-import { Controller, useFormContext } from 'react-hook-form';
-import { Autocomplete } from '@mui/material';
+import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
+import { Autocomplete, Box, TextField, Checkbox } from '@mui/material';
+import { Option } from '../types/option';
+import { default as CheckBoxOutlineBlankIcon } from '@mui/icons-material/CheckBoxOutlineBlank';
+import { default as CheckBoxIcon } from '@mui/icons-material/CheckBox';
 
-type Props = {
-	name: string;
-}
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  options: Option[];
+  label: string;
+};
 
-export const RHFAutocomplete = ({ name }: Props) => {
-	const { control } = useFormContext();
-	return (
-		<Controller
-			control={control}
-			name={name}
-			render={(params) => <Autocomplete />}
-		/>
-	);
-}
+export const RHFAutocomplete = <T extends FieldValues>({
+  name,
+  options,
+  label,
+}: Props<T>) => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { value, onChange, ref }, fieldState: { error } }) => (
+        <Autocomplete
+          options={options}
+          value={value?.map((id: string) =>
+            options.find((item) => item.id === id)
+          )}
+          getOptionLabel={(option) =>
+            options?.find((item) => item.id === option.id)?.label ?? ''
+          }
+          isOptionEqualToValue={(option, newValue) => option.id === newValue.id}
+          onChange={(_, newValue) => {
+            onChange(newValue.map((item) => item.id));
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              fullWidth
+              inputRef={ref}
+              error={!!error}
+              helperText={error?.message}
+              label={label}
+            />
+          )}
+          renderOption={(props, option, { selected }) => (
+            <Box component={'li'} {...props}>
+              <Checkbox
+                icon={<CheckBoxOutlineBlankIcon />}
+                checkedIcon={<CheckBoxIcon />}
+                checked={selected}
+              />
+              {option.label}
+            </Box>
+          )}
+          disableCloseOnSelect
+          multiple
+        />
+      )}
+    />
+  );
+};
--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -2,6 +2,7 @@ import { useFormContext } from 'react-hook-form';
 import { Stack, TextField } from '@mui/material';
 import { Schema } from '../types/schema';
 import { RHFAutocomplete } from '../../components/RHFAutocomplete';
+import { Option } from '../../types/option';
 
 export const Users = () => {
   const {
@@ -13,18 +14,23 @@ export const Users = () => {
     label: 'Name',
     error: !!errors.name,
     helperText: errors.name?.message,
-  }
+  };
   const emailProps = {
     ...register('email'),
     label: 'Email',
     error: !!errors.email,
     helperText: errors.email?.message,
-  }
+  };
+  const states: Option[] = [
+    { id: '1', label: 'California' },
+    { id: '2', label: 'Ohio' },
+    { id: '3', label: 'Texas' },
+  ];
   return (
     <Stack sx={{ gap: 2 }}>
       <TextField {...nameProps} />
       <TextField {...emailProps} />
-      <RHFAutocomplete name='states' />
+      <RHFAutocomplete<Schema> name="states" label="States" options={states} />
     </Stack>
   );
-}
+};
