cfccac5dd6a8e7aec4a6df6d0eb9cc1c605187a5
Kiran Anand <liteninkiran@hotmail.com>
2024-10-17 15:37:40 +0100

    Connect MUI, Zod and RHF

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,16 +1,29 @@
 import { useForm } from 'react-hook-form';
-import { TextField } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 import { schema, Schema } from '../types/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 export const Users = () => {
-  const { register } = useForm<Schema>({
+  const { register, formState: { errors } } = useForm<Schema>({
     mode: 'all',
     resolver: zodResolver(schema),
   });
+  const nameProps = {
+    ...register('name'),
+    label: 'Name',
+    error: !!errors.name,
+    helperText: errors.name?.message,
+  }
+  const emailProps = {
+    ...register('email'),
+    label: 'Email',
+    error: !!errors.email,
+    helperText: errors.email?.message,
+  }
   return (
-    <>
-      <TextField {...register('email')} />
-    </>
+    <Stack sx={{ gap: 2 }}>
+      <TextField {...nameProps} />
+      <TextField {...emailProps} />
+    </Stack>
   );
 }
--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 import { patterns } from '../../constants';
 
 export const schema = z.object({
-  name: z.string().min(1, { message: 'Required' }),
+  name: z.string().min(1, { message: 'Name is required' }),
   email: z.string()
     .min(1, { message: 'Email is required' })
     .refine((text) => patterns.email.test(text), {
